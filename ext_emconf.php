c8e8b95a949198d9b4d7d7faff76ca1febc315c9
laser <laser@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-12 14:02:42 +0000

    RC1, Live-Example @ mh_branchenbuch
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/mh_treeview/trunk@10040 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,44 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "mh_treeview"
+#
+# Auto generated 06-06-2008 13:34
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'Frontend Treeview',
+	'description' => '',
+	'category' => 'plugin',
+	'author' => 'Martin Hesse',
+	'author_email' => 'mail@martin-hesse.info',
+	'shy' => '',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'beta',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 0,
+	'lockType' => '',
+	'author_company' => '',
+	'version' => '0.1.0',
+	'constraints' => array(
+		'depends' => array(
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:8:{s:9:"ChangeLog";s:4:"5ad7";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:14:"ext_tables.php";s:4:"fe91";s:19:"doc/wizard_form.dat";s:4:"e3a0";s:20:"doc/wizard_form.html";s:4:"31ad";s:20:"static/constants.txt";s:4:"7612";s:16:"static/setup.txt";s:4:"f531";}',
+);
+
+?>
---
+++ res/tx_mhtreeview_functions.js
@@ -0,0 +1,14 @@
+<!--
+function tx_mhtreeview_toggle(val) {
+	var a = document.getElementById("tx_mhtreeview-lvl" + val);
+	var b = document.getElementById("tx_mhtreeview_toggleImg" + val);
+
+  if (a.style.display != 'none') {
+  		a.style.display = 'none';
+      b.src = 'typo3conf/ext/mh_treeview/res/plus.gif';
+  } else {
+  		a.style.display = 'block';
+  		b.src = 'typo3conf/ext/mh_treeview/res/minus.gif';
+  }
+}
+-->
